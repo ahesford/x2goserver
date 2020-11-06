e5660cdb032aeca8178358897cae475550aa73e7
Mihai Moldovan <ionic@ionic.de>
2020-11-06 06:34:53 +0100

    X2Go/Server/Agentt/: add new (stub) file NX/Options.pm as a library version of the options string manipulation code.

---
+++ X2Go/Server/Agent/NX/Options.pm
@@ -0,0 +1,20 @@
+# Copyright (C) 2018-2020 X2Go Project - https://wiki.x2go.org
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+
+package X2Go::Server::Agent::NX::Options;
+
+1;
